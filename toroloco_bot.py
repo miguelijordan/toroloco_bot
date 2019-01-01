16b76ac8d286d6f841e732eecab9803f2f9bde31
José Miguel Horcas Aguilera <horcas@lcc.uma.es>
2019-01-01 16:49:18 +0100

    Add info and error logging

--- toroloco_bot.py
+++ toroloco_bot.py
@@ -1,6 +1,7 @@
 import telebot
 import os
 import random
+import logging
 
 # CONSTANTS
 TOKEN = "714529585:AAHi_JisI_7ztewDLZeaoHsUUSrDRMlR2VY"
@@ -14,7 +15,7 @@ def load_chats():
         with(open(CHATS_DB, 'r')) as f:
             chats = set(map(int, f.read().splitlines()))
     except Exception as error:
-        print(error)
+        logging.error("Error loading chats.\n%s" % error)
         chats = set()
     return chats
 
@@ -25,14 +26,14 @@ def save_chats(chats):
             for c in chats:
                 f.write("%s\n" % c)
     except Exception as error:
-        print(error)
+        logging.error("Error saving chats.\n%s" % error)
 
 def get_photos():
     """ Load photos on a directory. """
     try:
         photos = os.listdir(PHOTOS_DIR)
     except Exception as error:
-        print(error)
+        logging.error("Error getting photos.\n%s" % error)
         photos = []
     return photos
 
@@ -40,9 +41,10 @@ def get_random_photo():
     """ Get a random photo from the photos directory. """
     try:
         photos = get_photos()
+        logging.info("#Photos: %i" % len(photos))
         sample = random.sample(photos, 1)[0]
     except Exception as error:
-        print(error)
+        logging.error("Error getting random photo.\n%s" % error)
         sample = None
     return sample
 
@@ -55,7 +57,7 @@ def send_photo(bot, chats, photo):
         try:
             bot.send_photo(c, image)
         except Exception as error:
-            print("Error: " + str(c))
+            logging.error("Invalid chat: %i.\n%s" % (c, error))
             print(error)
             invalid_chats.append(c)
     image.close()
@@ -63,16 +65,22 @@ def send_photo(bot, chats, photo):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='toro.log', filemode='a+', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s: %(message)s',)
+    logging.info("Toro Loco running...")
+
     bot = telebot.TeleBot(TOKEN)
 
     chats = load_chats()
     updates = bot.get_updates()
     chats |= set([u.message.chat.id for u in updates if u.message.chat.type == 'group'])
+    logging.info("#Group chats: %i" % len(chats))
 
     photo = get_random_photo()
-    print("Chats: " + str(chats))
-    print("Photo: " + str(photo))
     invalid_chats = send_photo(bot, chats, photo)
     chats -= invalid_chats
+    logging.info("#Photo (%s) sent to %i group chats." % (photo, len(chats)))
+    logging.info("#Invalid chats: %i" % len(invalid_chats))
 
     save_chats(chats)
+
+    logging.info("Toro Loco finished!")
